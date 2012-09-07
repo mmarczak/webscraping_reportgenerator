a1eca0977997a2708179e1d30c672240a14427f2
Michael <marczak.michael@gmail.com>
2012-09-07 15:58:57 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Youtube::Application.config.secret_token = '1ad87c736584aa0cd836d519da371553ac1075eaeeb4846b2c097a2fb49132eb9851a4cc95431cc401ca817cbe2cb066b390323b0e48e1c4c2c7776ecdb2b113'
